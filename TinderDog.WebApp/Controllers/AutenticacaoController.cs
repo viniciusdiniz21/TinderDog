07143728e5dbba4dc3a6536022dee3d3ce9ce82d
viniciusdiniz21 <vinicius.diniz444@gmail.com>
2023-09-05 21:48:09 -0300

    sla

---
+++ TinderDog.WebApp/Controllers/AutenticacaoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TinderDog.WebApp.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
---
+++ TinderDog.WebApp/Models/UsuarioViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TinderDog.WebApp.Models
+{
+    public class UsuarioViewModel
+    {
+        public string usuario { get; set; }
+        public string senha { get; set; }
+
+        public bool Autenticado()
+        {
+            if (usuario == "Teste" && senha == "123")
+                return true;
+            return false;
+        }
+    }
+}
